3628e562e6de5aad5d5f309b222f1a9deb277e09
mmuo3i3q3x7or8hj3326 <>
2026-02-25 20:21:31 -0500

    enhance run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,6 @@
 @echo off
 python -c "import PySide6, saxonche, pygments, lxml, darkdetect" 2>nul
 if %errorlevel% neq 0 (
-    echo Installing dependencies...
-    python -m pip install --upgrade pip
-    pip install -r requirements.txt
+    python -m pip install -r requirements.txt
 )
 start "" pythonw main.py
